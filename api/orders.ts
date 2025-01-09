85d99872ee893dc86d768ffcbec28c4f43ea13dd
filbertamadea <filbertamadea@gmail.com>
2025-01-10 02:41:43 +0700

    test

--- api/orders.ts
+++ api/orders.ts
@@ -1,7 +1,7 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { createClient } from "@supabase/supabase-js";
 
-console.log(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+console.log(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY, process.env);
 
 const supabase = createClient(
   process.env.SUPABASE_URL || "",
